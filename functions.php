36a0b7e337799185684946e60a4705e2a3008e3e
usammjekiqi <usammjekiqi@gmail.com>
2025-02-11 17:49:35 +0100

    n

--- functions.php
+++ functions.php
@@ -3,8 +3,8 @@
 
 
 function add_theme_scripts(){
+    wp_enqueue_style('style', get_template_directory_uri( ). "/style.css",false,'1.0',"all");
     
 }
-
-
+add_action("wp_enqueue_scripts", "add_theme_scripts");
 ?>
