c13667c86f3b9007f926de14d361e37eee89a21d
usammjekiqi <usammjekiqi@gmail.com>
2025-03-04 17:48:48 +0100

    n

--- functions.php
+++ functions.php
@@ -3,7 +3,9 @@
 function add_theme_script(){
     wp_enqueue_style( "style", get_template_directory_uri()."/style.css", false, '1.0', "all");    
     wp_enqueue_script("main", get_template_directory_uri()."/js/main.js", array('jquery'), "1.0", true);
+    wp_enqueue_script("bootstrap.bundle.min", get_template_directory_uri()."/js/bootstrap.bundle.min.js", array('jquery'), "1.0", true);
     wp_enqueue_style( "main", get_template_directory_uri()."/css/main.css", false, '1.0', "all");
+    wp_enqueue_style( "maibootstrap.min", get_template_directory_uri()."/css/bootstrap.min.css", false, '1.0', "all");
 
     if (is_singular() && comments_open() && get_option('thread-comments')) {
         wp_enqueue_script('comment-reply');
